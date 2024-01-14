02ca49f15f3c3047db034c9a90a8ed3675f5becb
Alberto Noguchi <alberto2611@gmail.com>
2024-01-14 00:28:36 -0300

    add: wiring beans

---
+++ maven-start/sq-ch3-ex6/src/main/java/beans/Person.java
@@ -0,0 +1,42 @@
+package beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private String name = "Ella";
+    private Parrot parrot;
+
+    public Person() {
+
+    }
+
+    public Person(String name, Parrot parrot) {
+        this.name = name;
+        this.parrot = parrot;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Autowired
+    public void setParrot(Parrot parrot) {
+        this.parrot = parrot;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+
+    @Override
+    public String toString() {
+        return "Parrot : " + name;
+    }
+
+}
---
+++ maven-start/sq-ch3-ex7/src/main/java/beans/Person.java
@@ -0,0 +1,26 @@
+package beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+public class Person {
+    private String name;
+    private Parrot parrot;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setParrot(Parrot parrot) {
+        this.parrot = parrot;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+
+}
