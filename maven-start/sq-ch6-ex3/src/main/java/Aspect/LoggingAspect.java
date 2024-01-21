115ae4721511956fb023328999059d8ccd336cfe
Alberto Noguchi <alberto2611@gmail.com>
2024-01-20 23:52:29 -0300

    add: using aspects with spring aop part 1

---
+++ maven-start/sq-ch6-ex3/src/main/java/Aspect/LoggingAspect.java
@@ -0,0 +1,33 @@
+package Aspect;
+
+import model.Comment;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+@Aspect
+public class LoggingAspect {
+
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+    @Around("execution(* services.*.*(..))")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        String methodName = joinPoint.getSignature().getName();
+        Object [] arguments = joinPoint.getArgs();
+
+        logger.info("Method " + methodName +
+                " with parameters " + Arrays.asList(arguments) +
+                " will execute");
+
+        Comment comment = new Comment();
+        comment.setText("Some other text!!!");
+        Object [] newArguments = {comment};
+
+        Object returnByMethod = joinPoint.proceed(newArguments);
+
+        logger.info("Method executed and returned " + returnByMethod);
+        return "FAILED";
+    }
+}
