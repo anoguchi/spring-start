bd9acd801bcb6b71f9b410e5ab21e97f71246220
Alberto Noguchi <alberto2611@gmail.com>
2024-01-27 20:53:00 -0300

    add: implementing rest services

---
+++ maven-start/sq-ch9-ex2/src/main/java/com/example/sqch9ex2/model/LoginProcessor.java
@@ -0,0 +1,48 @@
+package com.example.sqch9ex2.model;
+
+import com.example.sqch9ex2.services.LoggedUserManagementService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class LoginProcessor {
+
+    private final LoggedUserManagementService loggedUserManagementService;
+
+    private String username;
+    private String password;
+
+    public LoginProcessor(
+            LoggedUserManagementService loggedUserManagementService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+    }
+
+    public boolean login() {
+        String username = this.getUsername();
+        String password = this.getPassword();
+
+        boolean loginResult = false;
+
+        if ("julia".equals(username) && "qwe".equals(password)) {
+            loginResult = true;
+            loggedUserManagementService.setUsername(username);
+        }
+        return loginResult;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ maven-start/sq-ch9-ex3/src/main/java/com/example/sqch9ex3/model/LoginProcessor.java
@@ -0,0 +1,54 @@
+package com.example.sqch9ex3.model;
+
+import com.example.sqch9ex3.services.LoggedUserManagementService;
+import com.example.sqch9ex3.services.LoginCountService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class LoginProcessor {
+
+    private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
+
+    private String username;
+    private String password;
+
+    public LoginProcessor(
+            LoggedUserManagementService loggedUserManagementService,
+            LoginCountService loginCountService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
+    }
+
+    public boolean login() {
+        loginCountService.increment();
+        String username = this.getUsername();
+        String password = this.getPassword();
+
+        boolean loginResult = false;
+
+        if ("julia".equals(username) && "qwe".equals(password)) {
+            loginResult = true;
+            loggedUserManagementService.setUsername(username);
+        }
+        return loginResult;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
