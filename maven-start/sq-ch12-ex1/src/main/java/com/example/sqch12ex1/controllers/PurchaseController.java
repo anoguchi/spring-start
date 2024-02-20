04b4b3b5f0a4d7c85b67896cbe22a8dbda7456a1
Alberto Noguchi <alberto2611@gmail.com>
2024-02-20 02:52:56 -0300

    add: using jdbctemplate to work with persisted data

---
+++ maven-start/sq-ch12-ex1/src/main/java/com/example/sqch12ex1/controllers/PurchaseController.java
@@ -0,0 +1,26 @@
+package com.example.sqch12ex1.controllers;
+
+import com.example.sqch12ex1.model.Purchase;
+import com.example.sqch12ex1.repositories.PurchaseRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+    @GetMapping
+    public List<Purchase> findPurchases() {
+        return purchaseRepository.findAllPurchases();
+    }
+}
