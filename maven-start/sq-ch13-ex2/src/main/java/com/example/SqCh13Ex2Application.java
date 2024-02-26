6196da91a8b8b0a1c22bda3a5f264fd258d29b24
Alberto Noguchi <alberto2611@gmail.com>
2024-02-25 23:36:13 -0300

    add: using transactions simulating a problem to rollback

---
+++ maven-start/sq-ch13-ex2/src/main/java/com/example/SqCh13Ex2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh13Ex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh13Ex2Application.class, args);
+	}
+
+}
